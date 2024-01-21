a36bbf4ca35c48cf6ed95a1c7f798a55eb9eece9
bhoh <bhoh@mit.edu>
2024-01-21 14:42:05 -0600

    penalty progress

--- Diffusion/src/MatrixAssembly.cpp
+++ Diffusion/src/MatrixAssembly.cpp
@@ -16,7 +16,7 @@ DD GenerateQuadWeights(std::vector<double> &gpX, std::vector<double> &gpY, int n
 }
 
 
-DD StiffnessMatrix(QTM::QuadTreeMesh mesh, double k) {
+SpD StiffnessMatrix(QTM::QuadTreeMesh mesh, double k) {
     int deg = mesh.deg;
     int numNodes = deg+1;
     int numElemNodes = numNodes * numNodes;
@@ -24,7 +24,7 @@ DD StiffnessMatrix(QTM::QuadTreeMesh mesh, double k) {
     int nNodes = nElements * numNodes;
     std::vector<double> gaussPoints = Utils::genGaussPoints(deg);
 
-    // Generate derivative matrices
+    // Generate derivative matrix
     std::vector<double> AInitializer; 
     AInitializer.reserve(numElemNodes * numElemNodes);
 
@@ -68,8 +68,8 @@ DD StiffnessMatrix(QTM::QuadTreeMesh mesh, double k) {
     auto leaves = mesh.GetAllCells();
     for (auto &elm : leaves) {
         // calculate local matrix
-        localElemMat = combinedX.transpose()*coeffMat*weightMat*combinedX +
-                        combinedY.transpose()*coeffMat*weightMat*combinedY;
+        localElemMat = combineXT*coeffMat*weightMat*combinedX +
+                        combineYT*coeffMat*weightMat*combinedY;
  
         // Get nodes in element
         auto nodeBound = elm->nodes[0];
@@ -87,7 +87,7 @@ DD StiffnessMatrix(QTM::QuadTreeMesh mesh, double k) {
     return mat;
 }
 
-DD PenaltyMatrix(QTM::QuadTreeMesh mesh, double k) {
+DD PenaltyMatrix(QTM::QuadTreeMesh mesh, double k, double alpha) {
     int deg = mesh.deg;
     int numNodes = deg+1;
     int numElemNodes = numNodes * numNodes;
@@ -98,18 +98,36 @@ DD PenaltyMatrix(QTM::QuadTreeMesh mesh, double k) {
     auto leaves = mesh.GetAllCells();
     std::vector<QTM::Direction> directions = {QTM::Direction::N, QTM::Direction::E, 
                                                 QTM::Direction::S, QTM::Direction::W};
+    // Signs for element edges
+    int edgeSign[4] = {1,1,-1-1};
+    int cellSign; int neighborSign;
+    double a; // penalty parameters
+
+    std::vector<Eigen::Triplet<double>> tripletList;    
     for (auto &elm : leaves) {
         // get neighbors
         for (auto dir : directions) {
-            auto neighbors = elm->getNeighbor(dir);
+            cellSign = edgeSign[dir]; neighborSign = -cellSign;
+            auto neighbors = mesh.GetCellNeighbors(dir, elm->CID);
             auto elemNodes = mesh.GetBoundaryNodes(dir, elm->CID);
             for (auto neighbor : neighbors) {
                 if (neighbor->CID > elm->CID) {
                     continue;
-                } else if (neighbor->CID == -1) {
+                } else if (neighbor->CID == -1) { // case curr boundary is exterior
                     continue;
-                } else {
+                } else { // neighbor exists
+                    auto neighborNodes = mesh.GetBoundaryNodes((QTM::Direction)((dir+2)%4), neighbor->CID);
+                    DD localElemMat(elemNodes.size() + neighborNodes.size(), elemNodes.size() + neighborNodes.size());
+                    // calculate penalty param
+                    a = alpha/std::min(elm->width, neighbor->width)/2;
+                    elemNodes.insert(elemNodes.end(), neighborNodes.begin(), neighborNodes.end());
 
+                    for (int j=0; j<elemNodes.size(); j++) {
+                        for (int i=0; i<elemNodes.size(); i++) {
+                            tripletList.emplace_back(elemNodes[i],elemNodes[j],localElemMat(i,j));
+                        }
+                    }
+                    
                 }
             }
         }
@@ -125,7 +143,72 @@ DD FluxMatrix(QTM::QuadTreeMesh mesh, double k) {
     int nNodes = nElements * numNodes;
     std::vector<double> gaussPoints = Utils::genGaussPoints(deg);
     auto leaves = mesh.GetAllCells();
+
+    // Generate derivative matrix
+    std::vector<double> AInitializer; 
+    AInitializer.reserve(numElemNodes * numElemNodes);
+
+    double *AInitIdx = AInitializer.data(); 
+
+    // Generate derivatives for each basis function, copy to full array
+    for (int k=0; k<numNodes; k++) { 
+        std::vector<double> xPartials = Utils::numDeriv(.00001, k, gaussPoints, gaussPoints);
+        std::copy(xPartials.begin(), xPartials.end(), AInitIdx);
+        AInitIdx += numNodes;
+    }
+
     for (auto &elm : leaves) {
         // calculate local matrix
     }
+}
+
+SpD AssembleFVec(QTM::QuadTreeMesh mesh, double f, std::string evalStr) {
+    int deg = mesh.deg;
+    int numNodes = deg+1;
+    int numElemNodes = numNodes * numNodes;
+    int nElements = mesh.numLeaves;
+    int nNodes = nElements * numNodes;
+    std::vector<double> gaussPoints = Utils::genGaussPoints(deg);
+
+    // Generate quadrature weight matrices
+    DD weightMat = GenerateQuadWeights(gaussPoints, gaussPoints, numNodes, numNodes, numElemNodes);
+
+    // Turn weight mat int vector and mult. by source since diagonal
+    DvD sourceVec = f * weightMat.diagonal();
+
+    auto allNodesPos = mesh.AllNodePos();
+    auto startpoint = allNodesPos.data(); auto allocSize = allNodesPos.size();
+    auto fEval = Utils::EvalSymbolicBC(startpoint, allocSize, evalStr);
+
+    // Initialize i,j,v triplet list for sparse matrix
+    std::vector<Eigen::Triplet<double>> tripletList;
+    tripletList.reserve(nElements * numElemNodes);
+
+    // Integrate over all elements
+    DvD localElemMat(numElemNodes);
+    auto leaves = mesh.GetAllCells();
+    for (auto &elm : leaves) {
+        auto nodesInElm = elm->nodes;
+        double Lx = elm->width; // Jacobian factors
+        // calculate local matrix
+        std::vector<double> collectSourceVals; collectSourceVals.reserve(numElemNodes);
+
+        for (int i=nodesInElm[0]; i<=nodesInElm[1]; i++) {
+            collectSourceVals.push_back(fEval[i]);
+        }
+        Eigen::Map<DvD> sourceVector(collectSourceVals.data(), numElemNodes, 1);
+
+        localElemMat = weightMat*sourceVector*Lx*Lx/4;
+        // Get nodes in element
+        
+        // Generate i,j,v triplets
+        for (int i=0; i<numElemNodes; i++) {
+            tripletList.emplace_back(nodesInElm[i], 0, localElemMat(i));
+        }  
+    }
+
+    // Declare and construct sparse matrix from triplets
+    SpD mat(nNodes,1);
+    mat.setFromTriplets(tripletList.begin(), tripletList.end());
+    return mat;
 }
